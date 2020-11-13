4ed909d6a1c99e906fad6c4fb735f03ffce28d91
apurbamandal7 <apurbamandal147@gmail.com>
2020-11-13 11:26:26 +0530

    Add files via upload

---
+++ validating_a_url_by_format.py
@@ -0,0 +1,8 @@
+import re
+input_url = input("Enter a URL: ")
+regex = re.compile(
+        r'^(?:www|ftp)s://'
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
+        r'(?::\d+)?'
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
